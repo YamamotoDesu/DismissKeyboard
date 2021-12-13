0817ad4baacb616b2c1c93632de55cc5a3f6039b
yamamoto <kyoyamamoto1202@gmail.com>
2021-12-13 09:51:12 +0900

    Adding gif

--- DismissKeyboard/ViewController.swift
+++ DismissKeyboard/ViewController.swift
@@ -8,20 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        text1.delegate = self
-
+//        text1.delegate = self
+//        text2.delegate = self
+        
+        setupTextFields()
+        
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+        
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         text1.resignFirstResponder()
         
         return true
+    }
+    
+    func setupTextFields() {
+        let toolbar = UIToolbar()
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                        target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done,
+                                         target: self, action: #selector(doneButtonTapped))
+        
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+        toolbar.sizeToFit()
+        
+        text1.inputAccessoryView = toolbar
+        text2.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneButtonTapped() {
+        view.endEditing(true)
     }
 }
 
